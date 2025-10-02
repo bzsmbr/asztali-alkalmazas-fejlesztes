2696e6e0c2d48619cf78441a5d11ea2f64375dc8
Bálint Zsombor <balintzsombi070301@gmail.com>
2025-10-02 09:37:51 +0200

    fajlbeolvasas

---
+++ IO feladatok/04 - Konyvek/konyvek/konyvek/Program.cs
@@ -0,0 +1,47 @@
+﻿using konyvek;
+using System.Text;
+
+var fileData = await File.ReadAllLinesAsync("adatok.txt", Encoding.UTF7);
+
+var books = new List<Book>();
+
+foreach (var line in fileData)
+{
+    var data = line.Split('\t');
+    books.Add(new Book
+    {
+        FirstName = data[0],
+        LastName = data[1],
+        BirthDate = DateTime.Parse(data[2]),
+        Title = data[3],
+        ISBN = data[4],
+        Publisher = data[5],
+        ReleaseYear = int.Parse(data[6]),
+        Price = int.Parse(data[7]),
+        Theme = data[8],
+        NumberOfPages = int.Parse(data[9]),
+        Honorary = int.Parse(data[10])
+    });
+}
+
+//Írjuk ki a képernyőre az össz adatot
+Console.WriteLine($"1. feladat (ossz adat): \n");
+foreach (var book in books) {
+    Console.WriteLine(book);
+}
+
+//Keressük ki az informatika témajú könyveket és mentsük el őket az informatika.txt állömányba
+Console.WriteLine($"2. feladat (informatika tema): \n");
+var it = fileData.Where(x => x.Contains("informatika"));
+await File.WriteAllLinesAsync("informatika.txt", it, encoding: Encoding.UTF8);
+
+
+//Az 1900.txt állományba mentsük el azokat a könyveket amelyek az 1900-as években íródtak
+var booksInThe1900s = books.Where(x => x.ReleaseYear >= 1900)
+                           .Where(x => x.ReleaseYear < 2000).Select(x => x.ToFullyString());
+
+await File.WriteAllLinesAsync("1900.txt", booksInThe1900s, encoding: Encoding.UTF8);
+
+
+
+Console.ReadKey();
